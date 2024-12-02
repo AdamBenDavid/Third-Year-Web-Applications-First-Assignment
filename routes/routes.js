f868e47d3229fdfd20d0656418b2e246630ebfdc
Aviv <avivush2001@cs.colman.ac.il>
2024-12-02 21:09:34 +0200

    new updates

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const postsController = require("../controllers/posts_controller");
+
+router.get("/", postsController.getAllPosts);
+
+router.post("/", postsController.addPost);
+
+// router.get("/", postsController.getAllPosts);
+
+// router.get("/:id", postsController.getPostById);
+
+// router.post("/", postsController.createPost);
+
+// router.delete("/:id", postsController.deletePost);
+
+module.exports = router;
